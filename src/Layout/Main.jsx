07b015cdf3880358cff57ba38b9ea1c5ab16b85b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-18 12:14:52 +0600

    login and register page added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
@@ -9,6 +9,7 @@ const Main = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
+            <ScrollRestoration></ScrollRestoration>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 
 
@@ -11,7 +13,12 @@ const router = createBrowserRouter([
       errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
-            
+            path: '/login',
+            element: <Login></Login>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
         }
       ]
     },
