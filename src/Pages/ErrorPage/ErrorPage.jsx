bbec8078e132b512a04a8b20ffbea166df7dde73
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-18 08:49:03 +0600

    basic project setup done

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Lottie from "lottie-react";
+import ErrorAnimation from '../../assets/Error.json'
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <div className='mx-auto w-96'>
+                <Lottie animationData={ErrorAnimation} />
+            </div>
+            <div className="">
+                <div className="text-center">
+                    <p className="text-base font-semibold text-black">404</p>
+                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-5xl">
+                        Page not found
+                    </h1>
+                    <p className="mt-4 text-base leading-7 text-gray-600">
+                        Sorry, we couldn&apos:t find the page you&apos;re looking for.
+                    </p>
+                    <div className="mt-4 flex items-center justify-center gap-x-3">
+                        <Link>
+                            <button
+                                type="button"
+                                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+
+                            >
+
+                                Go back
+                            </button>
+
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>
+    },
+  ]);
+
+  export default router;
