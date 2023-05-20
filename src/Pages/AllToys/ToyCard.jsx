5401b45f82139aec5e8db77b1e36db7ce4dccd68
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-20 11:22:26 +0600

    AOS package used on the home page

--- src/Pages/AllToys/ToyCard.jsx
+++ src/Pages/AllToys/ToyCard.jsx
@@ -11,13 +11,18 @@ const ToyCard = ({ toy }) => {
                 src={picture}
                 alt="Laptop"
                 className="h-[200px] w-full rounded-t-md object-cover"
+                data-aos="zoom-in"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000"
             />
-            <div className="p-4">
+            <div className="p-4" data-aos="fade-up"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000" >
                 <h1 className="inline-flex items-center text-lg font-semibold">
                     {toyName} <ArrowUpRight className="ml-2 h-4 w-4" />
                 </h1>
                 <p className="mt-3 text-sm text-gray-600">
-                   Seller: {sellerName}
+                    Seller: {sellerName}
                 </p>
                 <div className="mt-4">
                     <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
     return (
         <div className="relative w-full bg-white">
             <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-                <div className="flex flex-col justify-center px-4 py-12 md:py-16 lg:col-span-7 lg:gap-x-6 lg:px-6 lg:py-24 xl:col-span-6">
+                <div className="flex flex-col justify-center px-4 py-12 md:py-16 lg:col-span-7 lg:gap-x-6 lg:px-6 lg:py-24 xl:col-span-6" data-aos="zoom-in"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="1000">
 
                     <h1 className="mt-8 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-6xl">
                         Welcome to Driven..
@@ -18,7 +20,7 @@ const Hero = () => {
 
                         <div>
                             <button
-                                type="button"
+                                type="button" 
                                 className="rounded-md bg-black px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                             >
                                 Buy Toys
